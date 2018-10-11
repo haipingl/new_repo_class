aa1125c93d6f48104296b01ce1d7d1ff5e42de80
haipingliu <haipingl@mtu.edu>
2018-10-10 22:25:49 -0400

    repo2

--- git_hub.py
+++ git_hub.py
@@ -2,3 +2,5 @@
 #python is an alian language for me
 #nomatter how hard i work on it, i am still lost during class
 #command+k: to commit, command+shift+k: to push
+
+#add new information, this week goes much faster
